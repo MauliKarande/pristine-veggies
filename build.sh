cdd15b8be026b6f54b2a77d921de631edfeab739
Mauli Prakash Karande <164602755+MauliKarande@users.noreply.github.com>
2026-01-04 21:14:15 +0530

    Auto create superuser on deploy for free Render

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
+python manage.py collectstatic --noinput
+
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+  python manage.py createsuperuser --noinput || true
+fi
